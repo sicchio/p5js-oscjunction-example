f1fc6b09ee4483275ed0da2d794cfe8ea2fdcbea
Kate <kate@sicchio.com>
2021-01-04 11:17:59 -0500

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -28,7 +28,7 @@ function draw() {
     ellipse(mouseX, mouseY, 20, 20);
     if(junction !== undefined && junction.connected) {
      // junction.send("/mouse", [mouseX / width, mouseY / height]);
-	     junction.send("/mouse", [1]);
+	     junction.send("/outputs/digital/16", [1]);
     }
   }
 }
